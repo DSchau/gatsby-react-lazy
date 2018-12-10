0165d8a2df5e459284a42114105561730d81aca7
Dustin Schau <dustinschau@gmail.com>
2018-12-10 12:56:52 -0600

    feat: initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,54 @@
+import React, { lazy, Suspense } from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+import './layout.css'
+
+const Header = lazy(() => import('./header'))
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+        </Suspense>
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
